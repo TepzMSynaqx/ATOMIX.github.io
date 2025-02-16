4f17877a7365744140487d6c9e4f7ffd59a82ec5
TepzMSynaqx <S43092@bj.ac.th>
2025-02-16 21:04:39 +0700

    Fixed Netlify build command

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import type { NodeOptions } from "sakulink";
 
-export const token: string = "MTMzOTk2NTQ0ODc3MDU1NTk0NQ.GwB0dS.IgLTp1wh-Oj_Ax9Vsg48thRGWTjB4sfroUBpKc";
+export const token: string = "MTMzOTk2NTQ0ODc3MDU1NTk0NQ.GuV03s.8GzF7l17xv6Ieyggu8OAvRtanQ1HBlC17MlnDg";
 export const nodes: NodeOptions[] = [
 	{
 		identifier: "OTARI TH",
